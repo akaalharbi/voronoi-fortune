2258f3f0ba35bcb394470570ef03b7565f1be137
Ahmed Alharbi <akaalharbi@protonmail.com>
2019-03-18 17:55:37 +0100

    incomplete  dcel and fortune

--- python/dcel.py
+++ python/dcel.py
@@ -19,11 +19,29 @@ class Vertex :
         return str(self.v)
 
 
-class halfEdge :
+class HalfEdge :
     
-    def __init__(self, origin = None, end = None,  twin = None,\
+    def __init__(self, origin = None,   twin = None,\
                  incidentFace = None, next = None, previous = None):
         """While running Fortune's we only add origin and the end. 
         Also, we get information about the incident face since a half-edge 
         seperates two sites."""
+        self.origin = origin #the vertex it originates from
+        self.end = None #this can be deduced from the twin
+        self.twin = twin #(-self.e[0], -self.e[1]) #the other direction
+        self.next = next
+        self.incidentFace = incidentFace
+        self.next = next
+        self.previous = previous 
+        
+
+class Face :
     
+    def __init__(self, p_i, incidentEdges = None,   ): 
+        self.p = p_i  
+        self.incidentEdges = incidentEdges
+
+class DCEL: 
+    
+    def __init__(self, vertices, )
+        
---
+++ python/fortune.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from avl import AVL
+from dcel import DCEL
+
+"""
+Created on Mon Mar 18 17:44:50 2019
+
+@author: Ahmed
+"""
+
+
+def vor_diag(sites):
+    #initialize
+    #Q, beachline, voronoi
+    #fill Q
+    #
+    
+    #while Q: #not empty
+    #event = Q.pop()
+    #if event is ... : handle_site(event, beachline, voronoi )
+    #else : handle_circle(event, beachline, voronoi)
+    pass
+
+def handle_site(event, beachline, voronoi):
+    pass
+
+def handle_Circle(eventevent, beachline, voronoi):
+    pass
+
